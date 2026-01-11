34f8874f504b7353cd28641dd47c13c89463c0df
jdarcyryan <james@darcyryan.co.uk>
2026-01-11 12:02:56 +0000

    added public + exported commands

--- .build/template/Template.psm1
+++ .build/template/Template.psm1
@@ -17,14 +17,14 @@ $classes | foreach {
     switch ($extension) {
         '.ps1' {
             # Process standard classes
-            . $_
+            . $currentClassPath
         }
         '.cs' {
             # Process CSharp classes
-            Add-Type -Path $_ -Language CSharp
+            Add-Type -Path $currentClassPath -Language CSharp
         }
         default {
-            throw "Unable to process class '$_', $exentsion is an unsupported file type."
+            throw "Unable to process class '$_', $extension is an unsupported file type."
         }
     }
 }
@@ -36,12 +36,37 @@ $classes | foreach {
 $privatePath = "$PSScriptRoot\private"
 
 $privatePath | Get-ChildItem -Filter '*.ps1' | where PSIsContainer -eq $false | foreach {
-    . $_
+    . $_.FullName
 }
 
 #endregion Private
 
 #region Public
 
+$publicPath = "$PSScriptRoot\public"
+
+# Alias snapshot
+$currentAlias = Get-Alias | select Name, ReferencedCommand
+
+# Function snapshot
+$currentCommands = (Get-Command).Name
+
+$publicPath | Get-ChildItem -Filter '*.ps1' | where PSIsContainer -eq $false | foreach {
+    . $_.FullName
+}
+
+# Exported commands and alias'
+$aliasToExport = Compare-Object $currentAlias (Get-Alias) -Property Name, ReferencedCommand |
+    where SideIndicator -eq '=>' |
+    select -ExpandProperty InputObject
+
+$commandsToExport = Compare-Object $currentCommands (Get-Command).Name |
+    where SideIndicator -eq '=>' |
+    where Name -notin $aliasToExport.ReferencedCommand |
+    select -ExpandProperty InputObject
+
+Export-ModuleMember -Function $aliasToExport.ReferencedCommand -Alias $aliasToExport.Name
+
+Export-ModuleMember -Function $commandsToExport.Name
 
 #endregion Public
