d7ba27f2af50aa09d162775abaf1590bf693bbd2
jdarcyryan <james@darcyryan.co.uk>
2026-01-11 22:57:08 +0000

    updates debugging

--- .build/scripts/Test-GitHubPackageVersion.ps1
+++ .build/scripts/Test-GitHubPackageVersion.ps1
@@ -59,8 +59,7 @@ function Test-GitHubPackageVersion {
     
     $nupkg = Get-ChildItem -Path '.output' -Filter '*.nupkg' | select -First 1
     
-    if (-not $nupkg)
-    {
+    if (-not $nupkg) {
         throw 'No .nupkg file found in .output directory.'
     }
     
@@ -68,12 +67,10 @@ function Test-GitHubPackageVersion {
     
     $filename = $nupkg.Name
     
-    if ($filename -match '\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-[\w\.-]+)?)\.nupkg$')
-    {
+    if ($filename -match '\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-[\w\.-]+)?)\.nupkg$') {
         $version = $Matches[1]
     }
-    else
-    {
+    else {
         throw "Could not extract version from filename '$filename'."
     }
     
@@ -86,57 +83,118 @@ function Test-GitHubPackageVersion {
     $headers = @{
         Authorization = "Bearer $Token"
         Accept        = 'application/vnd.github+json'
+        'X-GitHub-Api-Version' = '2022-11-28'
     }
     
     Write-Verbose 'Checking GitHub Packages...'
     
     try {
-        # Get all packages for the owner
-        $packagesUri = "https://api.github.com/users/$Owner/packages?package_type=nuget"
-        $packagesResponse = Invoke-RestMethod -Uri $packagesUri -Headers $headers -ErrorAction Stop
+        # Try multiple endpoints to find packages
+        $packagesUris = @(
+            "https://api.github.com/orgs/$Owner/packages?package_type=nuget",
+            "https://api.github.com/users/$Owner/packages?package_type=nuget",
+            "https://api.github.com/repos/$Owner/$Repository/packages?package_type=nuget"
+        )
         
-        # Find the specific package
-        $package = $packagesResponse | Where-Object { $_.name -eq $packageName }
+        $package = $null
+        $packagesResponse = $null
+        $workingUri = $null
+        
+        foreach ($packagesUri in $packagesUris) {
+            try {
+                Write-Verbose "Trying packages endpoint: $packagesUri"
+                $packagesResponse = Invoke-RestMethod -Uri $packagesUri -Headers $headers -ErrorAction Stop
+                
+                Write-Verbose "Response contains $($packagesResponse.Count) packages"
+                
+                # Find the specific package
+                $package = $packagesResponse | where { $_.name -eq $packageName }
+                
+                if ($package) {
+                    Write-Verbose "Found package '$packageName' in GitHub Packages at endpoint: $packagesUri"
+                    $workingUri = $packagesUri
+                    break
+                }
+                else {
+                    Write-Verbose "Package '$packageName' not found in this endpoint"
+                }
+            }
+            catch {
+                $statusCode = [int]$_.Exception.Response.StatusCode
+                if ($statusCode -eq 404) {
+                    Write-Verbose "Endpoint not found: $packagesUri"
+                    continue
+                }
+                else {
+                    throw
+                }
+            }
+        }
         
         if ($package) {
-            Write-Verbose "Found package '$packageName' in GitHub Packages."
-            
-            # Get package versions
-            $versionsUri = "https://api.github.com/users/$Owner/packages/nuget/$packageName/versions"
-            $versionsResponse = Invoke-RestMethod -Uri $versionsUri -Headers $headers -ErrorAction Stop
-            
-            $existingVersions = $versionsResponse | ForEach-Object { $_.name }
-            
-            if ($existingVersions -contains $version) {
-                throw "Version '$version' already exists in GitHub Packages."
+            # Determine the correct versions endpoint based on which packages endpoint worked
+            $versionsUri = $null
+            if ($workingUri -like '*orgs/*') {
+                $versionsUri = "https://api.github.com/orgs/$Owner/packages/nuget/$packageName/versions"
+            }
+            elseif ($workingUri -like '*users/*') {
+                $versionsUri = "https://api.github.com/users/$Owner/packages/nuget/$packageName/versions"
+            }
+            elseif ($workingUri -like '*repos/*') {
+                $versionsUri = "https://api.github.com/repos/$Owner/$Repository/packages/nuget/$packageName/versions"
             }
             
-            # Check if current version is later than latest existing version
-            if ($existingVersions.Count -gt 0) {
-                $latestVersion = $existingVersions | Sort-Object { [Version]($_ -replace '-.*$', '') } | Select-Object -Last 1
-                
-                $currentVersionParsed = [Version]($version -replace '-.*$', '')
-                $latestVersionParsed = [Version]($latestVersion -replace '-.*$', '')
-                
-                if ($currentVersionParsed -le $latestVersionParsed) {
-                    throw "Current version '$version' is not later than the latest existing version '$latestVersion' in GitHub Packages."
+            if ($versionsUri) {
+                try {
+                    Write-Verbose "Getting versions from: $versionsUri"
+                    $versionsResponse = Invoke-RestMethod -Uri $versionsUri -Headers $headers -ErrorAction Stop
+                    
+                    $existingVersions = $versionsResponse | foreach { $_.name }
+                    
+                    Write-Verbose "Found existing versions: $($existingVersions -join ', ')"
+                    
+                    if ($existingVersions -contains $version) {
+                        throw "Version '$version' already exists in GitHub Packages."
+                    }
+                    
+                    # Check if current version is later than latest existing version
+                    if ($existingVersions.Count -gt 0) {
+                        $latestVersion = $existingVersions | sort { [Version]($_ -replace '-.*$', '') } | select -Last 1
+                        
+                        $currentVersionParsed = [Version]($version -replace '-.*$', '')
+                        $latestVersionParsed = [Version]($latestVersion -replace '-.*$', '')
+                        
+                        if ($currentVersionParsed -le $latestVersionParsed) {
+                            throw "Current version '$version' is not later than the latest existing version '$latestVersion' in GitHub Packages."
+                        }
+                        
+                        Write-Verbose "Current version '$version' is later than latest existing version '$latestVersion'."
+                    }
+                    
+                    Write-Verbose "Version '$version' does not exist in GitHub Packages."
                 }
-                
-                Write-Verbose "Current version '$version' is later than latest existing version '$latestVersion'."
+                catch {
+                    $statusCode = [int]$_.Exception.Response.StatusCode
+                    if ($statusCode -eq 404) {
+                        Write-Verbose 'Package versions not found (empty package).'
+                    }
+                    else {
+                        throw
+                    }
+                }
             }
-            
-            Write-Verbose "Version '$version' does not exist in GitHub Packages."
-        } else {
+            else {
+                throw 'Could not determine correct versions endpoint.'
+            }
+        }
+        else {
             Write-Verbose 'Package not found in GitHub Packages (new package).'
         }
     }
     catch {
         $statusCode = [int]$_.Exception.Response.StatusCode
         
-        if ($statusCode -eq 404) {
-            Write-Verbose 'Package not found in GitHub Packages (new package).'
-        }
-        elseif ($statusCode -eq 401 -or $statusCode -eq 403) {
+        if ($statusCode -eq 401 -or $statusCode -eq 403) {
             throw 'Failed to connect to GitHub Packages. Authentication failed. Check your token permissions.'
         }
         else {
@@ -149,8 +207,7 @@ function Test-GitHubPackageVersion {
     
     Write-Verbose 'Checking GitHub Releases...'
     
-    try
-    {
+    try {
         $releasesResponse = Invoke-RestMethod -Uri $releasesUri -Headers $headers -ErrorAction Stop
         $existingReleaseTags = $releasesResponse | foreach {
             $_.tag_name
@@ -158,27 +215,22 @@ function Test-GitHubPackageVersion {
         
         $versionTag = $version
         
-        if ($existingReleaseTags -contains $version -or $existingReleaseTags -contains $versionTag)
-        {
+        if ($existingReleaseTags -contains $version -or $existingReleaseTags -contains $versionTag) {
             throw "Version '$version' already exists in GitHub Releases."
         }
         
         Write-Verbose "Version '$version' does not exist in GitHub Releases."
     }
-    catch
-    {
+    catch {
         $statusCode = [int]$_.Exception.Response.StatusCode
         
-        if ($statusCode -eq 404)
-        {
+        if ($statusCode -eq 404) {
             Write-Verbose 'No releases found (new repository).'
         }
-        elseif ($statusCode -eq 401 -or $statusCode -eq 403)
-        {
+        elseif ($statusCode -eq 401 -or $statusCode -eq 403) {
             throw 'Failed to connect to GitHub Releases, Authentication failed. Check your token permissions.'
         }
-        else
-        {
+        else {
             throw "Failed to connect to GitHub Releases: '$($_.Exception.Message)'."
         }
     }
