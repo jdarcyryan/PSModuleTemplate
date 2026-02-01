9079d8f34c6f5b50d66c755569baecaefb037aed
jdarcyryan <james@darcyryan.co.uk>
2026-02-01 17:00:43 +0000

    (temp) adding module files for build testing

---
+++ PSModuleTemplate/PSModuleTemplate.psm1
@@ -0,0 +1,103 @@
+#region Authoring
+
+###########################################################################
+# PSModuleTemplate
+# Author:  James D'Arcy Ryan
+# GitHub:  https://github.com/jdarcyryan/PSModuleTemplate
+# License: https://github.com/jdarcyryan/PSModuleTemplate/blob/main/LICENSE
+#
+# A standardized template for creating PowerShell modules with support for
+# classes (PowerShell and C#), private functions, and public functions with
+# automatic discovery and export of commands and aliases.
+#
+# LEGAL NOTICE:
+# The license referenced above applies to the PSModuleTemplate repository
+# and template structure only. Any module created using this template is
+# subject to its own license as specified in the module's LICENSE file,
+# which supersedes the template license for that specific module.
+###########################################################################
+
+#region Authoring
+
+#region Classes
+
+$classesPath = "$PSScriptRoot\classes"
+$classesDataFilePath = "$classesPath\classes.psd1"
+
+if (Test-Path -Path $classesDataFilePath) {
+    $classes = (Import-PowerShellDataFile -Path $classesDataFilePath).classes
+
+    $classes | foreach {
+        $currentClassPath = "$classesPath\$_"
+
+        if (!(Test-Path -Path $currentClassPath)) {
+            throw "Class '$_' does not exist."
+        }
+
+        $extension = (Get-Item -Path $currentClassPath).Extension
+
+        switch ($extension) {
+            '.ps1' {
+                # Process standard classes
+                . $currentClassPath
+            }
+            '.cs' {
+                # Process CSharp classes
+                Add-Type -Path $currentClassPath -Language CSharp
+            }
+            default {
+                throw "Unable to process class '$_', $extension is an unsupported file type."
+            }
+        }
+    }
+}
+
+#endregion Classes
+
+#region Private
+
+$privatePath = "$PSScriptRoot\private"
+
+if (Test-Path -Path $privatePath) {
+    Get-ChildItem -Path $privatePath -Filter '*.ps1' | where PSIsContainer -eq $false | foreach {
+        . $_.FullName
+    }
+}
+
+#endregion Private
+
+#region Public
+
+$publicPath = "$PSScriptRoot\public"
+
+if (Test-Path -Path $publicPath) {
+    # Snapshots
+    $currentAlias = Get-Alias | select Name, ReferencedCommand
+    $currentCommands = (Get-Command).Name
+
+    Get-ChildItem -Path $publicPath -Filter '*.ps1' | where PSIsContainer -eq $false | foreach {
+        . $_.FullName
+    }
+
+    # Find new aliases
+    $aliasToExport = Compare-Object $currentAlias (Get-Alias | select Name, ReferencedCommand) -Property Name, ReferencedCommand |
+        where SideIndicator -eq '=>' |
+        select -ExpandProperty InputObject
+
+    # Find new commands
+    $commandsToExport = Compare-Object $currentCommands (Get-Command).Name |
+        where SideIndicator -eq '=>' |
+        where InputObject -notin $aliasToExport.ReferencedCommand |
+        select -ExpandProperty InputObject
+
+    # Combine all functions and export
+    $allFunctions = @($commandsToExport) + @($aliasToExport.ReferencedCommand) | 
+        where { $_ } | 
+        select -Unique
+
+    if ($allFunctions -or $aliasToExport) {
+        Export-ModuleMember -Function $allFunctions -Alias $aliasToExport.Name
+    }
+}
+
+#endregion Public
---
+++ PSModuleTemplate/classes/classes.psd1
@@ -0,0 +1,8 @@
+@{
+    classes = @(
+        # ,'class1.ps1'
+        # ,'class2.cs'
+        # ,'class3.ps1'
+        # ,'class4.cs'
+    )
+}
