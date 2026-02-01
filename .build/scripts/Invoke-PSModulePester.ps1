7f9147a17d1fdcbe6c63cd7a56b1791ee73805bd
jdarcyryan <james@darcyryan.co.uk>
2026-02-01 16:52:34 +0000

    added pre pester build check

--- .build/scripts/Invoke-PSModulePester.ps1
+++ .build/scripts/Invoke-PSModulePester.ps1
@@ -17,6 +17,22 @@ function Invoke-PSModulePester {
 
     $gitRoot = Resolve-Path -Path "$PSScriptRoot\..\.."
     $pesterModulePath = "$PSScriptRoot\..\modules\Pester"
+    $moduleName = Split-Path -Path $gitRoot -Leaf
+    $outputModulePath = "$gitRoot\.output\$moduleName"
+
+    # Verify module has built
+    if (-not (Test-Path -Path $outputModulePath -PathType Container)) {
+        throw "Module directory not found at: '$outputModulePath', run 'make build' to build the module."
+    }
+    else {
+        try {
+            # Test module import before tests
+            . { Import-Module $outputModulePath }.GetNewClosure()
+        }
+        catch {
+            throw "Built module could not be imported at: '$outputModulePath', please run 'make build' to rebuild the module."
+        }
+    }
 
     # Verify Pester module exists
     if (-not (Test-Path -Path $pesterModulePath)) {
